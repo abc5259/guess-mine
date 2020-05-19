6f529019c50880db9d251d6cd0a5be5643ad0550
abc5259 <dlwogns3413@naver.com>
2020-05-19 23:31:02 +0900

    #4-1

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# guess-mine
-Realtime Game built with SocketIO
+# Guess Mine
+
+Realtime Drawing Game built with SocketIO, Gulp and Node
+
+## To Do:
+
+- [x] Server
+- [x] Pug
+- [x] Static
+- [ ] SocketIO
---
+++ src/server.js
@@ -0,0 +1,23 @@
+import { join } from "path";
+import express from "express";
+import socketIO from "socket.io";
+import logger from "morgan";
+import socketController from "./socketController";
+
+const PORT = 4000;
+const app = express();
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+app.use(logger("dev"));
+
+const handleListening = () =>
+  console.log(`✅ Server running: http://localhost:${PORT}`);
+
+app.get("/", (req, res) => res.render("home"));
+
+const server = app.listen(PORT, handleListening);
+
+const io = socketIO.listen(server);
+
+io.on("connection", (socket) => socketController(socket));
